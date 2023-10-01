8c14d61dd7765ea7dc0ae588a51fb7d2d5bdccd8
Jordan Kisiel <jordan.kisiel@gmail.com>
2023-10-01 16:28:35 -0400

    Added options to ActionButton

--- components/ActionButton.tsx
+++ components/ActionButton.tsx
@@ -1,10 +1,24 @@
 type Props = {
+    isWidthFull: boolean
+    bgColor: string
+    textColor: string
+    textSize: string
     children: React.ReactNode
 }
 
-export default function ActionButton({ children }: Props) {
+export default function ActionButton({
+    isWidthFull,
+    bgColor,
+    textColor,
+    textSize,
+    children,
+}: Props) {
     return (
-        <button className="bg-purple-600 py-2 px-4 rounded-full flex text-neutral-100 justify-center gap-1 items-center">
+        <button
+            className={`py-2 px-4 rounded-full flex justify-center gap-1 items-center font-bold ${
+                isWidthFull && "w-full"
+            } ${bgColor} ${textColor} ${textSize}`}
+        >
             {children}
         </button>
     )
