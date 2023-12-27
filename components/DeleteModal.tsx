7c228b40225de18d90972d2341796c9c4958c6ee
Jordan Kisiel <jordan.kisiel@gmail.com>
2023-12-26 23:20:52 -0500

    Reworked DeleteModal to accept a generic item (board or task)

--- components/DeleteModal.tsx
+++ components/DeleteModal.tsx
@@ -2,90 +2,65 @@ import { useState } from "react"
 import { useRouter } from "next/navigation"
 import ActionButton from "./ActionButton"
 import { deleteBoard, deleteTask } from "@/lib/dataUtils"
+import { useQuery } from "@tanstack/react-query"
+import { boardByIdOptions, taskByIdOptions } from "@/lib/queries"
 import { Board, Task } from "@/types"
-import { testUserId } from "@/testing/testingConsts"
 
-type BoardProps = {
-    isBoard: true
+type Props = {
+    isBoard: boolean
+    itemToDelete: Board | Task
     selectedBoardIndex: number
     setIsModalOpen: Function
     changeSelectedBoardIndex: Function
 }
 
-type TaskProps = {
-    isBoard: false
-    taskId: number | null
-    selectedBoardIndex: number
-    setIsModalOpen: Function
-}
-
-type Props = BoardProps | TaskProps
-
-export default function DeleteModal(props: Props) {
+export default function DeleteModal({
+    isBoard,
+    itemToDelete,
+    selectedBoardIndex,
+    setIsModalOpen,
+    changeSelectedBoardIndex,
+}: Props) {
     const router = useRouter()
 
     const [isSubmitted, setIsSubmitted] = useState<boolean>(false)
 
-    let itemToDelete: Board | Task | null = null
+    let userMessage = `Are you sure you want to delete the '${itemToDelete.title}
+        ' board? This action will remove all columns and tasks and cannot be reversed.`
 
-    if (props.isBoard) {
-        itemToDelete = boards[props.selectedBoardIndex]
-    } else {
-        const tasks = boards[props.selectedBoardIndex].columns
-            .map((column) => {
-                return column.tasks.map((task) => {
-                    return task
-                })
-            })
-            .flat()
-
-        itemToDelete = tasks.filter((task) => {
-            return task.id === props.taskId
-        })[0]
-    }
-
-    let userMessage = `Are you sure you want to delete the '${
-        itemToDelete ? itemToDelete.title : "No board selected"
-    }' board? This action will remove all columns and tasks and cannot be reversed.`
-
-    if (!props.isBoard) {
-        userMessage = `Are you sure you want to delete the '${
-            itemToDelete ? itemToDelete.title : "No task selected"
-        }' task and its subtasks? This action cannot be reversed.`
+    if (!isBoard) {
+        userMessage = `Are you sure you want to delete the '${itemToDelete.title}
+        ' task and its subtasks? This action cannot be reversed.`
     }
 
     async function handleDelete() {
         setIsSubmitted(true)
 
-        let deleteRes: Response | undefined
-
-        if (props.isBoard && itemToDelete) {
-            deleteRes = await deleteBoard(itemToDelete.id)
-        }
-
-        if (!props.isBoard && itemToDelete) {
-            deleteRes = await deleteTask(itemToDelete.id)
+        if (isBoard) {
+            await deleteBoard(itemToDelete.id)
+        } else {
+            await deleteTask(itemToDelete.id)
         }
 
         //revalidating all data regardless of whether
         //we're deleting a task or board
-        if (deleteRes && deleteRes.ok) {
-            mutate(boards, { revalidate: true })
+        // if (deleteRes && deleteRes.ok) {
+        //     mutate(boards, { revalidate: true })
+        // }
+
+        if (isBoard) {
+            changeSelectedBoardIndex(0)
         }
 
-        if (props.isBoard) {
-            props.changeSelectedBoardIndex(0)
-        }
+        setIsModalOpen(false)
 
-        props.setIsModalOpen(false)
-
-        router.push(`/?board=${props.selectedBoardIndex}`)
+        router.push(`/?board=${selectedBoardIndex}`)
     }
 
     return (
         <div className="flex flex-col gap-6 bg-neutral-100 dark:bg-neutral-700">
             <h4 className="font-bold text-red-300 text-lg">
-                {`Delete this ${props.isBoard ? "board" : "task"}?`}
+                {`Delete this ${isBoard ? "board" : "task"}?`}
             </h4>
             <p className="text-sm text-neutral-500 leading-6">{userMessage}</p>
             <div className="flex flex-col gap-4 md:flex-row">
@@ -107,7 +82,7 @@ export default function DeleteModal(props: Props) {
                     bgColor="bg-neutral-300 dark:bg-neutral-100"
                     textColor="text-purple-600"
                     textSize="text-sm"
-                    handler={() => props.setIsModalOpen()}
+                    handler={() => setIsModalOpen()}
                     isDisabled={isSubmitted}
                 >
                     Cancel
