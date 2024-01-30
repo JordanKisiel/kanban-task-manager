d1644429716cf93a13f54b2faf38f0e3231f22af
Jordan Kisiel <jordan.kisiel@gmail.com>
2024-01-30 18:05:39 -0500

    moved column colors to external config file

---
+++ lib/config.ts
@@ -0,0 +1,11 @@
+export const columnColors = [
+    "bg-[#49C4E5]",
+    "bg-[#8471F2]",
+    "bg-[#67E2AE]",
+    "bg-[#E2B867]",
+    "bg-[#E26767]",
+    "bg-[#6782E2]",
+    "bg-[#96E267]",
+    "bg-[#D7D143]",
+    "bg-[#C543C8]",
+]
