b2466c42fb57400cc2cbf597673b28951018181f
Jordan Kisiel <jordan.kisiel@gmail.com>
2024-03-18 21:20:46 -0400

    Added hover effect to task cards

--- components/app-elements/TaskCard.tsx
+++ components/app-elements/TaskCard.tsx
@@ -82,8 +82,9 @@ export default function TaskCard({
             >
                 <div
                     className="
-                flex flex-row justify-between bg-neutral-100 dark:bg-neutral-700 rounded py-5 px-4 
-                shadow-[0_4px_6px_0_rgba(54,78,126,0.10)] dark:shadow-none"
+                        flex flex-row justify-between bg-neutral-100 dark:bg-neutral-700 rounded py-5 px-4 
+                        shadow-[0_4px_6px_0_rgba(54,78,126,0.10)] dark:shadow-none hover:bg-neutral-200 
+                      dark:hover:bg-neutral-600"
                 >
                     <div>
                         <h4 className="font-bold dark:text-neutral-100">
